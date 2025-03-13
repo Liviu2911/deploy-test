39e2ef64212d1ac5601531a354f8bbc714985bc2
Liviu2911 <pirvanliviu29@gmail.com>
2025-03-14 00:10:33 +0200

    routes update

--- src/routes/post/index.tsx
+++ src/routes/post/index.tsx
@@ -1,14 +1,28 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import {
+  createFileRoute,
+  Link,
+  useNavigate,
+  useSearch,
+} from "@tanstack/react-router";
 
 export const Route = createFileRoute("/post/")({
   component: RouteComponent,
+  validateSearch: (search) => search,
 });
 
 function RouteComponent() {
+  const search = useSearch({ strict: false });
+  const navigate = useNavigate();
+  if (search.edit) {
+    navigate({ to: "/" });
+  }
   return (
     <>
       <h1>Post</h1>
       <Link to="/about">Back</Link>
+      <Link to="/post" search={{ edit: true }}>
+        Back
+      </Link>
     </>
   );
 }
